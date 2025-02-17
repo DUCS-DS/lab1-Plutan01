105aad6e0635539cfba6cfebc4f6795d93391533
Plutan01 <78388635+Plutan01@users.noreply.github.com>
2025-02-17 11:11:25 -0600

    Forgor

--- edit_me.py
+++ edit_me.py
@@ -4,9 +4,9 @@ def find_max(lst):
 
     current_max = lst[0]
 
-    #
-    #  YOUR CODE GOES HERE
-    #
+    for i in range(1, len(lst)):
+        if lst[i] > current_max:
+            current_max = lst[i];
 
     return current_max
 
